bb3ddbf2ddd9c8813338a6d57a0379fa8c76dfb2
Beautiiiii <95921996+Beautiiiii@users.noreply.github.com>
2023-08-05 14:48:28 +0700

    1.6

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -2,8 +2,8 @@
   <RouterLink to="/event/123">
     <div class="event-class">
       <div class="event-card">
-        <span>@{{ event.time }} on {{ event.date }}</span>
-        <h4>{{ event.title }}</h4>
+        <span>@{{ event?.time }} on {{ event?.date }}</span>
+        <h4>{{ event?.title }}</h4>
       </div>
     </div>
   </RouterLink>
