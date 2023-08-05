c591af716b95e333b744d191ec49f182025be656
Beautiiiii <95921996+Beautiiiii@users.noreply.github.com>
2023-08-05 17:09:17 +0700

    3.1

---
+++ real-world-vue/src/views/event/EventDetailView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { type EventItem } from '@/type'
+import EventService from '@/services/EventService'
+const event = ref<EventItem | null>(null)
+
+const props = defineProps({
+    id: String
+})
+
+EventService.getEventById(Number(props.id)).then((response) => {
+    event.value = response.data
+}).catch(error => {
+    console.log(error)
+})
+
+</script>
+<template>
+    <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <div id="nav">
+        <router-link :to="{ name: 'EventDetails', params: { id } }">Details</router-link>
+        |
+        <router-link :to="{ name: 'EventEdit', params: { id }}">Edit</router-link>
+    </div>
+    <p>{{ event.time }} on {{ event.date  }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+    </div>
+</template>
