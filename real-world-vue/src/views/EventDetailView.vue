4166bda3d87db2ea17f8fee4cadb83f3c7103934
Beautiiiii <95921996+Beautiiiii@users.noreply.github.com>
2023-08-05 14:35:09 +0700

    1.3

--- real-world-vue/src/views/EventDetailView.vue
+++ real-world-vue/src/views/EventDetailView.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { Ref } from 'vue'
-import type { EventItem  } from '@/type';
+import type { EventItem  } from '@/type'
+import EventService from '@/services/EventService'
 const event = ref<EventItem | null> (null)
+const id: Ref<number> = ref(123)
+EventService.getEventById(id.value)
+.then((response) => {
+    event.value = response.data
+}).catch(error => {
+    console.log(error)
+})
 </script>
 
 <template>
