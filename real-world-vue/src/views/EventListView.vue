67882f51f45a44bb5f4560aa92884ec5dbcaa4e5
Beautiiiii <95921996+Beautiiiii@users.noreply.github.com>
2023-08-05 15:14:38 +0700

    2.4

--- real-world-vue/src/views/EventListView.vue
+++ real-world-vue/src/views/EventListView.vue
@@ -4,12 +4,19 @@ import type { EventItem } from '@/type'
 import { ref } from 'vue'
 import type { Ref } from 'vue'
 import eventService from '@/services/EventService'
+import EventService from '@/services/EventService'
  
 const events: Ref<Array<EventItem>> = ref([])
-  eventService.getEvent().then((response) => {
+const props = defineProps({
+  page: {
+    type: Number,
+    required: true
+  }
+})
+  
+  EventService.getEvent(2, props.page).then((response: AxiosResponse<EventItem[]>) => {
     events.value = response.data
   })
-  
 </script>
 
 <template>
